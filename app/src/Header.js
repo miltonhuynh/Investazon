ff3ae555978e2395cb9371c3b5bc2263ffd70306
miltonhuynh <miltonhuynh@gmail.com>
2023-08-23 22:19:09 -0700

    test

---
+++ app/src/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+import logo from './images/logo.jpg'
+
+function Header() {
+    return (
+        <div className="header">
+            <img src={logo} alt="investazon logo" />
+            <div className="headerSearch">
+                <input className="headerSearchInput" type="text" />
+            </div>
+            <div className="headerNav">
+                <div className="headerOption">
+
+                </div>
+                <div className="headerOption">
+
+                </div>
+                <div className="headerOption">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjv4XsLBepVBMttzIFPTFgXWCkhhDnBPA",
+  authDomain: "stockazon-d0997.firebaseapp.com",
+  projectId: "stockazon-d0997",
+  storageBucket: "stockazon-d0997.appspot.com",
+  messagingSenderId: "181041395539",
+  appId: "1:181041395539:web:e5ba012d97182dd75313a6"
+};
