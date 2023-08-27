b15366a1bb333e5567595234b6a8fc1af02c3864
miltonhuynh <miltonhuynh@gmail.com>
2023-08-27 14:53:48 -0700

    Added basket and styled search bar for header

--- app/src/Header.js
+++ app/src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Header.css'
 import logo from './images/logo.jpg'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 
 function Header() {
     return (
@@ -8,6 +10,7 @@ function Header() {
             <img className="headerLogo" src={logo} alt="investazon logo" />
             <div className="headerSearch">
                 <input className="headerSearchInput" type="text" />
+                <SearchIcon className="headerSearchIcon" />
             </div>
             <div className="headerNav">
 
@@ -33,6 +36,13 @@ function Header() {
                     </span>
                     <span className="headerOptionLineTwo">
                         Prime
+                    </span>
+                </div>
+
+                <div className="headerOptionBasket">
+                    <ShoppingCartCheckoutIcon />
+                    <span className="headerOptionLineTwo headerBasketCount">
+                        0
                     </span>
                 </div>
             </div>
