7c966c0a3b439f9a41bad1f5430142d149bc9ab4
miltonhuynh <miltonhuynh@gmail.com>
2023-08-31 11:15:20 -0700

    added dummy array to simulate API request

--- app/src/Home.js
+++ app/src/Home.js
@@ -3,11 +3,41 @@ import "./Home.css"
 import Stock from './Stock';
 
 function Home() {
+
+  const Items = [
+    {
+      "id":1,
+      "company":"Apple",
+      "ticker":"AAPL",
+      "price": 300
+    },
+    {
+      "id":2,
+      "company":"Google",
+      "ticker":"GOOG",
+      "price": 500
+    }
+  ]
+
+  console.log(Items)
+
+  const StocksList = Items.map((single) =>
+  <Stock
+    key={single.id}
+    company={single.company}
+    ticker={single.ticker}
+    price={single.image}  
+  />
+);
+
   return (
     <div className="home">
-      <div className="homeContainer">
+      {/*<div className="homeContainer">
         <Stock company="Apple" ticker="AAPL" price="300"/>
+        <Stock company="Google" ticker="GOOG" price="500"/>
         </div>
+  */}
+    {StocksList}
       </div>
   )
 }
