1f4171fe5f63a374d16c1cf0df868acf1c25a47c
miltonhuynh <miltonhuynh@gmail.com>
2023-09-02 14:55:08 -0700

    router bug

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Routes, Router, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
