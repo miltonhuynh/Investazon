1b8bd875ef6d1e0a893f80c465bb71b041fa2453
miltonhuynh <miltonhuynh@gmail.com>
2023-09-02 10:34:14 -0700

    Unable to fix local image pass as src bug

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,43 +1,46 @@
 import React from 'react';
 import "./Home.css"
 import Stock from './Stock';
+import appleLogo from './images/appleLogo.png'
 
 function Home() {
-
-
-  
 
   const Items = [
     {
       "id": 1,
       "company": "Apple",
       "ticker": "AAPL",
-      "price": 183.47
-    },
+      "price": 183.47,
+      "image": "\"./images/appleLogo.png\""
+    }/*,
     {
       "id": 2,
       "company": "Google",
       "ticker": "GOOG",
-      "price": 136.84
+      "price": 136.84,
+      "image": {appleLogo}
     },
     {
       "id": 3,
       "company": "Microsoft",
       "ticker": "MSFT",
-      "price": 328.66
+      "price": 328.66,
+      "image": {appleLogo}
     },
     {
       "id": 4,
       "company": "Netflix",
       "ticker": "NFLX",
-      "price": 439.88
+      "price": 439.88,
+      "image": {appleLogo}
     },
     {
       "id": 5,
       "company": "Amazon",
       "ticker": "AMZN",
-      "price": 138.12
-    }
+      "price": 138.12,
+      "image": {appleLogo}
+    }*/
   ]
 
   const StocksList = Items.map((single) =>
@@ -46,6 +49,7 @@ function Home() {
       company={single.company}
       ticker={single.ticker}
       price={single.price}
+      image={single.image}
     />
   );
 
