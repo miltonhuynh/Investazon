21d02533539ed0413fe7d2b971b817d7d17b6b21
miltonhuynh <miltonhuynh@gmail.com>
2023-09-02 21:30:30 -0700

    added checkout page

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <div className="App">
       <Header />
         <Routes>
-          <Route path="/" element={<Home />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/Checkout" element={<Checkout />}>
           </Route>
         </Routes>
       </div>
