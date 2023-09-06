0b4f37ae205ae280f01e6f76a7e338c5430c5d02
miltonhuynh <miltonhuynh@gmail.com>
2023-09-06 14:54:54 -0700

    finished implementing basket

--- app/src/Stock.js
+++ app/src/Stock.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import './Stock.css'
+import { useStateValue } from "./StateProvider"
+function Stock({id, company, ticker, price, image}) {
 
-function Stock({company, ticker, price, image}) {
+  const [{basket}, dispatch] = useStateValue();
+  console.log(basket)
 
-  /* "./images/appleLogo.png" */
-  console.log(company)
-  console.log(ticker)
-  console.log(price)
-  console.log(image)
+  const addToBasket = () => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        company: company,
+        ticker: ticker,
+        price: price,
+        image: image
+      }
+    })
+  }
 
   return (
     <div id="stock">
@@ -15,7 +25,8 @@ function Stock({company, ticker, price, image}) {
       <p id="ticker">{ticker}</p>
       <p id="price">${price}</p>
       <img src={image} alt="logo of company"/>
-      <button>Add to basket</button>
+
+      <button onClick={addToBasket}>Add to basket</button>
     </div>
   )
 }
