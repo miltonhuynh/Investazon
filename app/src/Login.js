05e97df4e03ed26c009e4452b52754807f377792
miltonhuynh <miltonhuynh@gmail.com>
2023-09-16 10:32:01 -0700

    working on firebase auth issue

--- app/src/Login.js
+++ app/src/Login.js
@@ -1,39 +1,47 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import './index.css'
-import loginLogo from './images/loginLogo.jpg' 
+import loginLogo from './images/loginLogo.jpg'
 import { Link } from "react-router-dom"
+import { auth } from "./firebase"
+
 
 function Login() {
-  
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  
+
   const signIn = e => {
     e.preventDefault()
   }
 
   const register = e => {
     e.preventDefault()
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth)
+      })
+      .catch(error => alert(error.message))
   }
-  
+
   return (
     <div id="login">
-        <Link to='/'>
-            <img id="loginLogo" src={loginLogo} alt="investazon logo" />
-        </Link>
-        <div id="loginContainer">
-            <h1>Sign-in</h1>
-            <form>
-                <h5>E-mail</h5>
-                <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+      <Link to='/'>
+        <img id="loginLogo" src={loginLogo} alt="investazon logo" />
+      </Link>
+      <div id="loginContainer">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
 
-                <h5>Password</h5>
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+          <h5>Password</h5>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 
-                <button type="submit" onClick={signIn} id="signInButton">Sign-in</button>
-            </form>
-            <button id="registerButton" conClick={register}>Create your Investazon account</button>
-        </div>
+          <button type="submit" onClick={signIn} id="signInButton">Sign-in</button>
+        </form>
+        <button id="registerButton" conClick={register}>Create your Investazon account</button>
+      </div>
     </div>
   )
 }
--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "./firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCjv4XsLBepVBMttzIFPTFgXWCkhhDnBPA",
   authDomain: "stockazon-d0997.firebaseapp.com",
@@ -6,3 +8,9 @@ const firebaseConfig = {
   messagingSenderId: "181041395539",
   appId: "1:181041395539:web:e5ba012d97182dd75313a6"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+export { db, auth };
