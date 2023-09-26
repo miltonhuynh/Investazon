47d54e39c225183e020d28694951fd9bd155a9d4
miltonhuynh <miltonhuynh@gmail.com>
2023-09-25 21:13:00 -0700

    added payment page template

--- app/src/Payment.js
+++ app/src/Payment.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Header from './Header';
 
 function Payment() {
   return (
-    <div>Payment</div>
+    <>
+      <Header />
+      <div className="paymentPage">
+        <div className="paymentContainer">
+          <div className="paymentSection">
+            <div className="paymentTitle">
+              <h3>Delivery Address</h3>
+            </div>
+            <div className="paymentAddress">
+
+            </div>
+          </div>
+          <div className="paymentSection">
+            
+          </div>
+          <div className="paymentSection">
+            
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
