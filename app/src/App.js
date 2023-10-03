980b76390d8ae32c42c91554f40700ecefec3ec3
miltonhuynh <miltonhuynh@gmail.com>
2023-10-02 19:37:47 -0700

    Imported loadStripe and Elements to app.js page, also added stripe public key

--- app/src/App.js
+++ app/src/App.js
@@ -7,6 +7,10 @@ import Login from './Login';
 import Payment from './Payment';
 import { auth } from "./firebase"
 import { useStateValue } from "./StateProvider"
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
+
+const promise = loadStripe("pk_test_51NwxXHBnZOkmnGWGKvHCBsz596PibSsxxT03ab7dL1I6u3aOn3QVDmLPPOvujYWQsjoFJUQSNkrtw7AsL2vnscbw00v8SEMAvZ")
 
 function App() {
   const[, dispatch] = useStateValue()
