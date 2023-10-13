e7edb3bfa3c25125212e6b80d03ede1e322f6f15
miltonhuynh <miltonhuynh@gmail.com>
2023-10-12 19:28:05 -0700

    added post method to getClientSecret function on payment page

--- app/src/Payment.js
+++ app/src/Payment.js
@@ -24,7 +24,10 @@ function Payment() {
 
   useEffect(() => {
     const getClientSecret = async () => {
-      const response = await axios
+      const response = await axios({
+        method: 'post',
+        url: `/payments/create?total=${getBasketTotal(basket)}`
+      }
     }
     getClientSecret()
   }, [basket])
